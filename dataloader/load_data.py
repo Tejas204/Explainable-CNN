f69fde3ac3df50c743aea507bbbca7113751150d
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-12-15 16:57:40 +0100

    Data Loader

--- dataloader/load_data.py
+++ dataloader/load_data.py
@@ -7,15 +7,10 @@ import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 
 
-# Define transforms
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# Load Dataset: CIFAR
-train_dataset = torchvision.datasets.CIFAR10(root="/Users/tejasdhopavkar/Documents/DL/Explainable_CNN/data", train=True, transform=transform, download=True)
-test_dataset = torchvision.datasets.CIFAR10(root="/Users/tejasdhopavkar/Documents/DL/Explainable_CNN/data", train=False, transform=transform, download=True)
 
 class LoadData(Dataset):
-    def __init__(self, data, labels, transform=transform):
+    def __init__(self, transform):
         """
         Docstring for __init__
         
@@ -26,8 +21,6 @@ class LoadData(Dataset):
         """
         super(LoadData, self).__init__()
         self.transform = transform
-        self.data = data
-        self.labels = labels
     
     def __len__(self):
         """
@@ -49,3 +42,22 @@ class LoadData(Dataset):
         if self.transform:
             sample = self.transform(sample)
         return sample, label
+    
+    def load_data(self, dataset:str):
+        """
+        Docstring for load_data
+        
+        :param self: Description
+        :param dataset: Description
+        :type dataset: str
+        """
+        if dataset == "CIFAR10":
+            training_data = torchvision.datasets.CIFAR10(root="/Users/tejasdhopavkar/Documents/DL/Explainable_CNN/data/CIFAR10", train=True, transform=self.transform, download=True)
+            testing_data = torchvision.datasets.CIFAR10(root="/Users/tejasdhopavkar/Documents/DL/Explainable_CNN/data/CIFAR10", train=False, transform=self.transform, download=True)
+        elif dataset == "CIFAR100":
+            training_data = torchvision.datasets.CIFAR100(root="/Users/tejasdhopavkar/Documents/DL/Explainable_CNN/data/CIFAR100", train=True, transform=self.transform, download=True)
+            testing_data = torchvision.datasets.CIFAR100(root="/Users/tejasdhopavkar/Documents/DL/Explainable_CNN/data/CIFAR100", train=False, transform=self.transform, download=True)
+        return training_data, testing_data
+
+
+
--- main.py
+++ main.py
@@ -7,5 +7,13 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from config.config import batch_size
+from dataloader.load_data import LoadData
 
+
+# Define transforms
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# Load Dataset
+data = LoadData(transform=transform)
+training_data, testing_data = data.load_data("CIFAR10")
 
