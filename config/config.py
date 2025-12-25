0af2908ef4be1c56ac4986f99409b1ccac354e1e
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-12-25 20:50:58 +0100

    Updated config

--- config/config.py
+++ config/config.py
@@ -7,12 +7,15 @@ import torch.nn.functional as F
 batch_size = 20
 
 # Experiment 1
-CNN_Config = {
-    "input_size": 28,
-    "output_size": 3,
-    "num_classes": 10,
-    "activation": nn.ReLU,
-    "normalization": True,
-    "drop_prob": 0.4,
-    "hidden_dims": [64, 128, 256, 128, 64]
-}
+CNN_Config = dict(
+    name = "CNN_Config_1",
+
+    model_args = dict(
+        input_size = 3,
+        num_classse = 10,
+        hidden_layers = [64, 128, 256, 128, 64],
+        activation = nn.ReLU,
+        norm_layer = False,
+        drop_prob = 0.4
+    )
+)
--- model/cnn.py
+++ model/cnn.py
@@ -6,10 +6,10 @@ import torch.nn.functional as F
 
 
 class ExplainableCNN(nn.Module):
-    def __init__(self, input_size: int, output_size: int, num_classes: int, activation, normalization: bool, drop_prob = 0.0):
+    def __init__(self, input_size, hidden_layers, num_classes, activation, normalization, drop_prob = 0.0):
         super(ExplainableCNN, self).__init__()
         self.input_size = input_size
-        self.output_size = output_size
+        self.output_size = hidden_layers
         self.num_classes = num_classes
         self.activation = activation
         self.norm = normalization
