996d84ee5ad28d6c1f26a242f099c7592e777072
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-12-28 21:43:37 +0100

    Changed model architecture

--- model/cnn.py
+++ model/cnn.py
@@ -20,35 +20,61 @@ class ExplainableCNN(nn.Module):
 
 
     def build_model(self):
-        layers = []
+        self.conv1 = nn.Conv2d(3, 64, 3, stride=1, padding=1)
+        self.norm1 = nn.BatchNorm2d(64)
 
-        input_dims = self.input_size
-        for i in range(len(self.hidden_layers)):
-            # Convolution
-            layers.append(nn.Conv2d(input_dims, self.hidden_layers[i], 3, stride=1, padding=1))
+        self.conv2 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.norm2 = nn.BatchNorm2d(128)
 
-            # Batch Norm
-            if self.norm:
-                layers.append(self.norm(self.hidden_layers[i]))
+        self.conv3 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.norm3 = nn.BatchNorm2d(256)
 
-            # Max pooling
-            if self.max_pool and i == len(self.hidden_layers[i]) - 2:
-                layers.append(nn.MaxPool2d((2, 2), stride=2))
+        self.conv4 = nn.Conv2d(256, 128, 3, stride=1, padding=1)
+        self.norm4 = nn.BatchNorm2d(128)
+
+        self.conv5 = nn.Conv2d(128, 64, 3, stride=1, padding=1)
+        self.norm5 = nn.BatchNorm2d(64)
+
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear(64*32*32, 10)
+
+        # layers = []
+
+        # input_dims = self.input_size
+        # for i in range(len(self.hidden_layers)):
+        #     # Convolution
+        #     layers.append(nn.Conv2d(input_dims, self.hidden_layers[i], 3, stride=1, padding=1))
+
+        #     # Batch Norm
+        #     if self.norm:
+        #         layers.append(self.norm(self.hidden_layers[i]))
+
+        #     # Max pooling
+        #     if self.max_pool and i == len(self.hidden_layers[i]) - 2:
+        #         layers.append(nn.MaxPool2d((2, 2), stride=2))
             
-            # Activation
-            layers.append(self.activation())
+        #     # Activation
+        #     layers.append(self.activation())
 
-            # Drop probabilities
-            if self.drop_prob:
-                layers.append(nn.Dropout(self.drop_prob))
+        #     # Drop probabilities
+        #     if self.drop_prob:
+        #         layers.append(nn.Dropout(self.drop_prob))
 
-            input_dims = self.hidden_layers[i]
+        #     input_dims = self.hidden_layers[i]
 
-        layers.append(nn.Flatten())
-        layers.append(nn.Linear(input_dims*32*32, self.num_classes))
-        self.layers = nn.Sequential(*layers)
+        # layers.append(nn.Flatten())
+        # layers.append(nn.Linear(input_dims*32*32, self.num_classes))
+        # self.layers = nn.Sequential(*layers)
+        # nn.Sequential()
 
     
     def forward(self, x):
-        output = self.layers(x)
-        return output
+        # output = self.layers(x)
+        x = F.relu(self.norm1(self.conv1(x)))
+        x = F.relu(self.norm2(self.conv2(x)))
+        x = F.relu(self.norm3(self.conv3(x)))
+        x = F.relu(self.norm4(self.conv4(x)))
+        x = F.relu(self.norm5(self.conv5(x)))
+        x = self.flatten(x)
+        x = self.linear(x)
+        return x
