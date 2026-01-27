ead1b8e94c80b47516ef32813181a4bed77764a3
Tejas204 <tejasdhopavkar2000@gmail.com>
2026-01-28 00:13:24 +0100

    Added heatmap featrue

--- utils/explanations.py
+++ utils/explanations.py
@@ -16,7 +16,7 @@ class Explanations():
     def arrange(self, h:int, w:int):
         self.arranged_tensor = torch.reshape(self.average_activations, (h, w))
 
-    def overlap_heatmap(self, image, activation, alpha_scale, cmap='jet', greyscale = False, single_feature=False):
+    def overlap_heatmap(self, image, activation, alpha_scale, cmap='jet', greyscale = False):
         if torch.is_tensor(image):
             image = image.detach().cpu().numpy()
             image = np.transpose(image, (1, 2, 0))
@@ -31,26 +31,37 @@ class Explanations():
         act = activation - activation.min()
         act = act / (act.max() + 1e-8)
 
-        if not single_feature:
-            colormap = plt.get_cmap(cmap)
-            heatmap = colormap(act)[..., :3]
+ 
+        colormap = plt.get_cmap(cmap)
+        heatmap = colormap(act)[..., :3]
 
-            alpha = (act * alpha_scale)[..., None]
+        alpha = (act * alpha_scale)[..., None]
 
-            # Blend
-            # overlay = (1 - alpha) * image + alpha * heatmap
-            overlay = image*(1-alpha) + alpha*heatmap
-            overlay = np.clip(overlay, 0, 1)
-        else:
-            rows = 32
-            columns = 32
-            
-            fig, axes = plt.subplots(rows, columns, figsize=(columns * 2, rows * 2))
-            axes = axes.flatten()
-
-            for i in range(columns):
-                axes[i].imshow(act[i].cpu(), cmap="viridis")
-                axes[i].axis("off")
-                axes[i].set_title(f"Ch {i}", fontsize=8)
-
+        # Blend
+        # overlay = (1 - alpha) * image + alpha * heatmap
+        overlay = image*(1-alpha) + alpha*heatmap
+        overlay = np.clip(overlay, 0, 1)
+       
         return overlay
+    
+    def overlap_heatmap_per_pixel(self, image, activation, xpos, ypos, alpha_scale, cmap='jet', greyscale = False):
+        if torch.is_tensor(image):
+            image = image.detach().cpu().numpy()
+            image = np.transpose(image, (1, 2, 0))
+            if greyscale:
+                image = rgb2gray(image)
+                image = np.stack([image, image, image], axis=-1)
+
+        if torch.is_tensor(activation):
+            activation = activation.detach().cpu().numpy()
+
+        
+        colormap = plt.get_cmap(cmap)
+        heatmap = colormap(activation)[..., :3]
+
+        alpha = (activation * alpha_scale)[..., None]
+
+        # Blend
+        overlay = image[xpos][ypos]*(1-alpha) + alpha*heatmap
+        overlay = np.clip(overlay, 0, 1)
+        
