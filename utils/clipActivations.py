7bf61d56a0703116dceccd601df8b9ec6a42b68c
Tejas204 <tejasdhopavkar2000@gmail.com>
2026-02-03 00:14:39 +0100

    CLIP Embeddings

--- utils/clipActivations.py
+++ utils/clipActivations.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import clip
+import json
 
 class Clip():
     def __init__(self, model, concept_file, device, class_list):
@@ -9,23 +10,27 @@ class Clip():
         self.concept_file = concept_file
         self.device = device
         self.class_list = class_list
+        self.class_concept_embeddings = {}
 
     # Modify to read concepts from json iteratively
     def collect_concepts(self):
         with open(self.concept_file, "r") as file:
-            self.concepts = file.read().split("\n")
+            self.concepts = json.load(file)
     
     def collect_embeddings(self):
         clip_model, processes = clip.load(self.model, device=self.device)
-        text_inputs = clip.tokenize(self.concepts)
 
-        with torch.no_grad():
-            text_inputs = text_inputs.to(self.device)
-            text_features = clip_model.encode_text(text_inputs)
+        for label in self.concepts.keys():
+            text_inputs = clip.tokenize(self.concepts[label])
 
-        text_features /= text_features.norm(dim=-1, keepdim=True)
-        print(text_features.shape)
+            with torch.no_grad():
+                text_inputs = text_inputs.to(self.device)
+                text_features = clip_model.encode_text(text_inputs)
+
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            self.class_concept_embeddings[label] = text_features
+        print(self.class_concept_embeddings)
     
     # Create file paths for the embedding files
     def save_embeddings(self):
-        pass
+        print()
